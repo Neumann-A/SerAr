204d7a64722c879709cf04bfb5d0a7e0209201ee
Alexander Neumann <Alexander.Neumann@hamburg.de>
2020-09-03 00:36:57 +0200

    fix compile definitions

--- SerAr.target.setup.cmake
+++ SerAr.target.setup.cmake
@@ -1,26 +1,25 @@
 
-#SerAr_WITH_ConfigFile
-#SerAr_WITH_ProgramOptions
-#SerAr_WITH_MATLAB
-#SerAr_WITH_HDF5
-#SerAr_WITH_Cereal
-
 if(SerAr_WITH_ConfigFile)
     target_link_libraries(AllArchives INTERFACE $<TARGET_NAME:ConfigFile>)
+    target_compile_definitions(AllArchives INTERFACE SERAR_HAS_CONFIGFILE)
 endif()
 
 if(SerAr_WITH_ProgramOptions)
     target_link_libraries(AllArchives INTERFACE $<TARGET_NAME:ProgramOptionsWrapper>)
+    target_compile_definitions(AllArchives INTERFACE SERAR_HAS_PROGRAMOPTIONS)
 endif()
 
 if(SerAr_WITH_MATLAB)
     target_link_libraries(AllArchives INTERFACE $<TARGET_NAME:MATLAB>)
+    target_compile_definitions(AllArchives INTERFACE SERAR_HAS_MATLAB)
 endif()
 
 if(SerAr_WITH_HDF5)
     target_link_libraries(AllArchives INTERFACE $<TARGET_NAME:HDF5>)
+    target_compile_definitions(AllArchives INTERFACE SERAR_HAS_HDF5)
 endif()
 
 if(SerAr_WITH_Cereal)
     target_link_libraries(AllArchives INTERFACE $<TARGET_NAME:CerealWrappers)
+    target_compile_definitions(AllArchives INTERFACE SERAR_HAS_CEREAL)
 endif()
