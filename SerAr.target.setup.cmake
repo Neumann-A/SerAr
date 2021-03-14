d43689d317ee0302bd014d3a295469557004e1c0
Alexander Neumann <Alexander.Neumann@hamburg.de>
2021-03-14 23:07:36 +0100

    switch to cmakejson

--- SerAr.target.setup.cmake
+++ SerAr.target.setup.cmake
@@ -1,25 +1,25 @@
 
-if(SerAr_WITH_ConfigFile)
+if(SERAR_WITH_CONFIGFILE)
     target_link_libraries(AllArchives INTERFACE $<TARGET_NAME:ConfigFile>)
     target_compile_definitions(AllArchives INTERFACE SERAR_HAS_CONFIGFILE)
 endif()
 
-if(SerAr_WITH_ProgramOptions)
+if(SERAR_WITH_PROGRAMOPTIONS)
     target_link_libraries(AllArchives INTERFACE $<TARGET_NAME:ProgramOptionsWrapper>)
     target_compile_definitions(AllArchives INTERFACE SERAR_HAS_PROGRAMOPTIONS)
 endif()
 
-if(SerAr_WITH_MATLAB)
+if(SERAR_WITH_MATLAB)
     target_link_libraries(AllArchives INTERFACE $<TARGET_NAME:MATLAB>)
     target_compile_definitions(AllArchives INTERFACE SERAR_HAS_MATLAB)
 endif()
 
-if(SerAr_WITH_HDF5)
+if(SERAR_WITH_HDF5)
     target_link_libraries(AllArchives INTERFACE $<TARGET_NAME:HDF5>)
     target_compile_definitions(AllArchives INTERFACE SERAR_HAS_HDF5)
 endif()
 
-if(SerAr_WITH_Cereal)
+if(SERAR_WITH_CEREAL)
     target_link_libraries(AllArchives INTERFACE $<TARGET_NAME:CerealWrappers)
     target_compile_definitions(AllArchives INTERFACE SERAR_HAS_CEREAL)
 endif()
