e111b5cb8186e161b3167b998ebd30e5c8308162
Alexander Neumann <Alexander.Neumann@hamburg.de>
2021-11-06 22:09:20 +0100

    string_view is not directly supported by serialization

--- components/Core/include/MyCEL_MetaInfoSerializer.hpp
+++ components/Core/include/MyCEL_MetaInfoSerializer.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <string>
 #include <MyCEL/types/metainfo.hpp>
 #include <SerAr/Core/NamedValue.h>
 
@@ -7,14 +8,14 @@ namespace MyCEL {
 
     template<typename Archive>
     void serialize(build_metainfo& info,Archive& ar) {
-        ar(SerAr::createNamedValue("date",info.date));
-        ar(SerAr::createNamedValue("time",info.time));
+        ar(SerAr::createNamedValue("date",std::string(info.date)));
+        ar(SerAr::createNamedValue("time",std::string(info.time)));
     }
 
     template<typename Archive>
     void serialize(git_metainfo& info,Archive& ar) {
-        ar(SerAr::createNamedValue("branch",info.branch));
-        ar(SerAr::createNamedValue("sha",info.sha));
-        ar(SerAr::createNamedValue("description",info.describe));
+        ar(SerAr::createNamedValue("branch",std::string(info.branch)));
+        ar(SerAr::createNamedValue("sha",std::string(info.sha)));
+        ar(SerAr::createNamedValue("description",std::string(info.describe)));
     }
 }
